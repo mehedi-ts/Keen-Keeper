296d8210b72dcdf0ecc5f93c43ca05f1e830f028
mehedi hasan <mehedihasan.j135@gmail.com>
2026-04-17 16:59:57 +0600

    fix some error

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -3,11 +3,13 @@ import Navbar from "../Components/Layout/Navbar";
 import { Outlet } from "react-router";
 import Footer from "../Components/Layout/Footer";
 import { ToastContainer } from "react-toastify";
+import Scroll from "../Components/Scroll";
 
 const MainLayout = () => {
   return (
     <>
       <div className="bg-[#F8FAFC]">
+        <Scroll></Scroll>
         <Navbar></Navbar>
         <Outlet></Outlet>
         <Footer></Footer>
